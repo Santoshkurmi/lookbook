4e062b9bbbfc0db175d46eb8b5e24d2747643c32
Santosh kurmi <xantosh121@gmail.com>
2023-03-10 16:36:09 +0545

    Created server from scratch using http modules of node

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+import http from "http"
 
 
+const server = http.createServer((req, res) => {
+  res.write("HEllo")
+  res.write("Hello world")
+  res.write("This is the world")
+  res.end()
+  console.log("Hello baby")
+})
 
+server.listen(5000, () => console.log("Server is listening on port 5000"))
 
 
