5881e2971e7f74428eaa86176b8698086fe3b204
Santosh kurmi <xantosh121@gmail.com>
2023-03-11 13:13:17 +0545

    used express router to modulalize our routes

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-import http from "http"
+import express from "express"
 
 
-const server = http.createServer((req, res) => {
-  res.write("HEllo")
-  res.write("Hello world")
-  res.write("This is the world")
-  res.end()
-  console.log("Hello baby")
-})
-
-server.listen(5000, () => console.log("Server is listening on port 5000"))
+const server = express()
 
 
